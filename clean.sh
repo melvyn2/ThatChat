89637245233b17cf74cf6c1ba407016979bc9895
melvyn2 <melvyn.depeyrot@gmail.com>
2018-03-29 15:05:48 -0700

    New directory structure

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname ${BASH_SOURCE[0]})"
-echo "Removing .pyc files..."
-find *.pyc -exec rm -f {}
-echo "done\n"
-echo "Cleaning \"dist\" and \"build\" directories..."
-rm -rf dist/* build/*
-echo "done\n"

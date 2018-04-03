21c76638eff3701f5cba6d0878f37f5c79848734
melvyn2 <melvyn.depeyrot@gmail.com>
2018-04-03 07:00:01 -0700

    added install step, removed nonexistant multithreading

--- ci_tools/build_pyqt.bat
+++ ci_tools/build_pyqt.bat
@@ -54,4 +54,7 @@ REM --qmake $HOME/miniconda/bin/qmake --sip $HOME/miniconda/bin/sip --verbose
 echo "(f) Compiling PyQt in %CD%"
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
 cd "C:\projects\pyqt\PyQt5_gpl-%PYQT_VER%"
-nmake -j 4
+nmake
+
+echo "(g) Installing PyQt"
+nmake install
